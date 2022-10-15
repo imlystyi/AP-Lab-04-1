4c546b8e64781254add055f1251a3f90a96379a7
imlystyi <vladte3c@gmail.com>
2022-10-15 19:30:42 +0300

    some changes of code

--- AP_Lab_04_1/Lab_04_1.cs
+++ AP_Lab_04_1/Lab_04_1.cs
@@ -27,7 +27,7 @@ namespace AP_Lab_04_1
                 ii++;
             }
 
-            Console.WriteLine($"Отримана першим способом сума \"S\": {S};");
+            Console.WriteLine($"Отримана першим способом сума \"S\":\t{S}");
             ii = 1; S = 0;
 
             // Спосіб 2: do{ … } while(…)
@@ -38,21 +38,21 @@ namespace AP_Lab_04_1
             }
             while (ii <= N);
 
-            Console.WriteLine($"отримана другим способом сума \"S\": {S};");
+            Console.WriteLine($"Отримана другим способом сума \"S\":\t{S}");
             S = 0;
 
             // Спосіб 3: for(…; …; n++) {… }
             for (ii = 1; ii <= N; ii++) 
                 S += Math.Sqrt(1 + Math.Pow(Math.Cos(Math.Sin(ii)), 2)) / (1 + Math.Pow(Math.Sin(Math.Cos(ii)), 2));
 
-            Console.WriteLine($"отримана третім способом сума \"S\": {S};");
+            Console.WriteLine($"Отримана третім способом сума \"S\":\t{S}");
             S = 0;
 
             // Спосіб 4: for(…; …; n--) {… }
             for (ii = N; ii >= 1; ii--)
                 S += Math.Sqrt(1 + Math.Pow(Math.Cos(Math.Sin(ii)), 2)) / (1 + Math.Pow(Math.Sin(Math.Cos(ii)), 2));
 
-            Console.WriteLine($"отримана четвертим способом сума \"S\": {S}");
+            Console.WriteLine($"Отримана четвертим способом сума \"S\":\t{S}");
 
             Console.ReadLine();
         }
